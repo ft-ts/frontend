d2169ab8fe3eb49d74dbd99184eaa24d60831784
jeongmino <jeongmino1207@gmail.com>
2023-07-28 21:03:46 +0900

    feat: profile background and profile image display

--- src/app/main/right-wrapper/page.tsx
+++ src/app/main/right-wrapper/page.tsx
@@ -1,9 +1,26 @@
 import './right-wrapper.css'
+import styles from './user-profile.module.css'
+import Image from "next/image"
 
 export default function RightWrapper() {
   return (
     <div id="right-wrapper">
       <h1>RightWrapper</h1>
+      <UserProfileBackground />
     </div>
   )
+}
+
+const UserProfileBackground = (props: {}) => {
+    return (
+        <div className={styles.userProfileBackground}>
+            <h1>hihi</h1>
+            <Image src="/asset/profile_dumy.png" 
+            className={styles.userProfilePicture}
+            width={200}
+            height={162}
+            quality={100}
+             alt="userPicture"/>
+        </div>
+    )
 }
