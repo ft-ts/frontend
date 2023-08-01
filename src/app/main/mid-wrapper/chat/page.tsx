362be6356fdddf4cb4c560ec4991f42a82976d8b
jeongmino <jeongmino1207@gmail.com>
2023-08-02 01:10:37 +0900

    feat: chat message background, Lobbymessage made

--- src/app/main/mid-wrapper/chat/page.tsx
+++ src/app/main/mid-wrapper/chat/page.tsx
@@ -1,9 +1,52 @@
-import './chat-wrapper.css'
+import styles from "./chat-wrapper.module.scss"
+import React from "react";
+import Image from "next/image";
 
 export default function ChatWrapper() {
   return (
-    <div id="chat-wrapper">
+    <div className={styles.chatWrapper}>
       <h1>ChatWrapper</h1>
+      <ChatBackground />
     </div>
   )
+}
+
+interface messageDecoProps{
+    left: string;
+}
+
+const ChatBackground = (props: {}) => {
+    return (
+        <div className={styles.chatBackground}>
+            <div className={styles.chatBackgroundBorder}></div>
+            <ChatSettingButton />
+            <LobbyNewMessage />
+        </div>
+    )
+}
+
+const ChatSettingButton = (props: {}) => {
+    return (
+        <button className={styles.settingButton}>
+            <Image 
+                src="/asset/setting.svg"
+                alt="settingButton"
+                width={44.45}
+                height={39}
+                />
+        </button>
+    )
+}
+
+
+
+const LobbyNewMessage = (props: {}) => {
+    return (
+        <div>
+            <h2 className={styles.LobbyMessageFont}>New message</h2>
+            <span className={styles.lobbyMessageBar} style={{left: '50px'}}></span>
+            <span className={styles.lobbyMessageBar} style={{left: '563px'}}></span>
+        </div>
+        
+    )
 }
