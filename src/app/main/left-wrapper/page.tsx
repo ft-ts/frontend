b3033f8e0d966617ac65d4fb7fee0e5559d25c8f
jeongmino <jeongmino1207@gmail.com>
2023-08-03 01:31:16 +0900

    feat: simple game mode create, started to make rightWarpper

--- src/app/main/left-wrapper/page.tsx
+++ src/app/main/left-wrapper/page.tsx
@@ -1,9 +1,18 @@
-import './left-wrapper.css'
+import styles from "./left-wrapper.module.scss"
 
 export default function LeftWrapper() {
   return (
-    <div id="left-wrapper">
+    <div className={styles.leftWrapper}>
       <h1>LeftWrapper</h1>
+      <CreateChannel />
     </div>
   )
+}
+
+const CreateChannel = (props: {}) => {
+    return (
+        <button className={styles.createChannelBox}>
+            <h2 className={styles.createChannelFont}>Create Channel</h2>
+        </button>
+    )
 }
--- src/app/main/mid-wrapper/game/page.tsx
+++ src/app/main/mid-wrapper/game/page.tsx
@@ -1,4 +1,5 @@
 import styles from "./game-wrapper.module.scss"
+import Image from "next/image"
 
 export default function GameWrapper() {
   return (
@@ -12,7 +13,9 @@ export default function GameWrapper() {
 const GameBackground = (props: {}) => {
     return (
         <div className={styles.gameBackground}>
+            <div className={styles.gameDisplayBackground}></div>
             <GameMatch />
+            <GameRecordSearch />
         </div>
     )
 }
@@ -22,5 +25,21 @@ const GameMatch = (props: {}) => {
         <button className={styles.gameMatchButton}>
             <h2 className={styles.gameMatchFont}>Game Matching</h2>
         </button>
+    )
+}
+
+const GameRecordSearch = (props: {}) => {
+    return (
+        <div>
+            <input className={styles.gameRecordSearchBox}></input>
+            <Image
+                className={styles.gameRecordIcon}
+                src="asset/RecordSearch.svg"
+                alt="RecordSearch icon"
+                width={26}
+                height={26}
+                ></Image>
+        </div>
+        
     )
 }
