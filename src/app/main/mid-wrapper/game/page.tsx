3a655ab073f3889bf6aee98e2bb603b889d1fa9b
jeongmino <jeongmino1207@gmail.com>
2023-08-02 16:23:03 +0900

    game background, button make

--- src/app/main/mid-wrapper/game/page.tsx
+++ src/app/main/mid-wrapper/game/page.tsx
@@ -1,9 +1,26 @@
-import './game-wrapper.css'
+import styles from "./game-wrapper.module.scss"
 
 export default function GameWrapper() {
   return (
-    <div id="game-wrapper">
+    <div className={styles.midWrapper}>
       <h1>GameWrapper</h1>
+      <GameBackground />
     </div>
   )
+}
+
+const GameBackground = (props: {}) => {
+    return (
+        <div className={styles.gameBackground}>
+            <GameMatch />
+        </div>
+    )
+}
+
+const GameMatch = (props: {}) => {
+    return (
+        <button className={styles.gameMatchButton}>
+            <h2 className={styles.gameMatchFont}>Game Matching</h2>
+        </button>
+    )
 }
