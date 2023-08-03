4b0cb088bc246d49e5ba5967581f78142ee22b1b
jeongmino <jeongmino1207@gmail.com>
2023-08-03 14:07:20 +0900

    feat: channel pannel made

--- src/app/main/left-wrapper/page.tsx
+++ src/app/main/left-wrapper/page.tsx
@@ -5,6 +5,7 @@ export default function LeftWrapper() {
     <div className={styles.leftWrapper}>
       <h1>LeftWrapper</h1>
       <CreateChannel />
+      <ChannelPannel />
     </div>
   )
 }
@@ -15,4 +16,12 @@ const CreateChannel = (props: {}) => {
             <h2 className={styles.createChannelFont}>Create Channel</h2>
         </button>
     )
+}
+
+const ChannelPannel = (props: {}) => {
+    return (
+        <div className={styles.channelPannelBox}>
+            <h2 className={styles.channelPannelFont}>Channels</h2>
+        </div>
+    )    
 }
