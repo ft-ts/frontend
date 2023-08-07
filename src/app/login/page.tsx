d8a79fd559cba6d2b105f0aa582bb45a078b2e4a
jeongmino <jeongmino1207@gmail.com>
2023-08-07 16:42:45 +0900

    feat: login background made

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,23 @@
 import Link from "next/link";
+import styles from "./login.module.scss";
 
 export default function Login() {
 
   return (
-    <div id="login">
-      <h1>Login</h1>
-      <Link href="/main">Main</Link>
+    <div className={styles.background}>
+        <GameBackground />
     </div>
   )
+}
+
+
+const GameBackground = (props: {}) => {
+    return (
+        <div className={styles.gameBackground}>
+            <div className={styles.displayBackground}>
+                <h1 className={styles.gameFont}>42 Login</h1>
+                <h2 className={styles.pressFont}>Press Start Button</h2>
+            </div>
+        </div>
+    )
 }
