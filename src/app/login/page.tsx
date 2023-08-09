b12d4a3ac193c51ed925fe1d7410e135d813172e
jeongmino <jeongmino1207@gmail.com>
2023-08-09 18:14:46 +0900

    feat: login background making

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import styles from "./login.module.scss";
+import Image from "next/image";
 
 export default function Login() {
 
   return (
     <div className={styles.background}>
         <GameBackground />
+        <GameCrossDeco />
+        <GameRoundRectangleDeco />
     </div>
   )
 }
@@ -18,6 +21,39 @@ const GameBackground = (props: {}) => {
                 <h1 className={styles.gameFont}>42 Login</h1>
                 <h2 className={styles.pressFont}>Press Start Button</h2>
             </div>
+        </div>
+    )
+}
+
+const GameCrossDeco = () => {
+    return (
+        <Image
+        src="/asset/Union.svg"
+        alt="game cross button deco"
+        width={300}
+        height={300}
+        className={styles.gameCrossDeco}
+        ></Image>
+    )
+}
+
+const GameRoundRectangleDeco = () => {
+    return (
+        <div className={styles.container}>
+            <Image
+                src="/asset/RoundRectangleButton.svg"
+                alt="RoundButtonRectangleDecoA"
+                width={180}
+                height={67}
+                className={styles.GameRoundRectangleDecoA}>
+            </Image>
+            <Image
+                src="/asset/RoundRectangleButton.svg"
+                alt="RoundButtonRectangleDecoB"
+                width={180}
+                height={67}
+                className={styles.GameRoundRectangleDecoB}>
+            </Image>
         </div>
     )
 }
