6823a4e3a48c61e0642a8244803aa440b8deb7de
김서우 <4nyth4me@gmail.com>
2023-08-29 19:07:04 +0900

    wip : pong game v0, need refact

--- src/app/main/mid-wrapper/game/page.tsx
+++ src/app/main/mid-wrapper/game/page.tsx
@@ -1,53 +1,13 @@
 "use client";
+
 import styles from './game-wrapper.module.scss'
 import Image from 'next/image'
+import GameItem from './gameItem';
 
 export default function GameWrapper() {
   return (
     <div className={styles.midWrapper}>
-      <GameBackground />
+        <GameItem />
     </div>
   )
-}
-
-const GameBackground = (props: {}) => {
-    return (
-        <div className={styles.gameBackground}>
-            <div className={styles.gameDisplayBackground}></div>
-            <GameMatch />
-            <GameRecordSearch />
-        </div>
-    )
-}
-
-const GameMatch = (props: {}) => {
-    const handleClick = () => {
-        alert("Game Matching")
-    }
-    return (
-        <button 
-        className={styles.gameMatchButton}
-        onClick={handleClick}
-        >
-            <h2 className={styles.gameMatchFont}>Game Matching</h2>
-        </button>
-    )
-}
-
-const GameRecordSearch = (props: {} | any) => {
-    return (
-        <div>
-            <input className={styles.gameRecordSearchBox}></input>
-            <button className={styles.gameRecordSearchButton}>
-            <Image
-                className={styles.gameRecordIcon}
-                src="asset/RecordSearch.svg"
-                alt="RecordSearch icon"
-                width={43}
-                height={43}
-                ></Image>
-            </button>
-        </div>
-        
-    )
 }
