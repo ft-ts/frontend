3415ad130621ce048d21e65924f528601cb0332f
Dohyun Lee <surimi.sake@gmail.com>
2023-09-01 18:41:22 +0900

    removed comment main

--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -28,46 +28,9 @@ export async function getUserList(): Promise<any> {
 }
 
 export async function getUserByUid(uid: number): Promise<UserInterface> {
-    return apiClient.get(`/users/${uid}`, {
-        headers: {
-            // "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-    });
+    return apiClient.get(`/users/${uid}`);
 }
 
 export async function getGameHistory(name: string): Promise<any> {
-    return apiClient.get(`/pong/${name}`, {
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-    })
+    return apiClient.get(`/pong/${name}`);
 }
-
-
-// // Auth
-// export async function signInApi(email, password) {
-//     return apiClient.post("/auth/signin", { email, password });
-// };
-
-// export async function signUpApi(email, password) {
-//     return apiClient.post("/auth/signup", { email, password });
-// };
-
-// // Todo
-// export async function getTodoApi() {
-//     return apiClient.get("/todos");
-// };
-
-// export async function postTodoApi(todo) {
-//     return apiClient.post("/todos", {todo});
-// };
-
-// export async function updateTodoApi(id, body) {
-//     return apiClient.put(`/todos/${id}`, body);
-// };
-
-// export async function deleteTodoApi(id) {
-//     return apiClient.delete(`/todos/${id}`);
-// };
