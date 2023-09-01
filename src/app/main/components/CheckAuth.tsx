9688800e91008d2b64920b095cffe9211021c138
Dohyun Lee <surimi.sake@gmail.com>
2023-09-01 18:24:58 +0900

    change socket auth header -> auth.token

--- src/app/main/components/CheckAuth.tsx
+++ src/app/main/components/CheckAuth.tsx
@@ -1,23 +1,17 @@
+'use client'
 import { io } from "socket.io-client";
-import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 
 export const socket = io("http://localhost:10000", {
-  autoConnect: false,
-  extraHeaders: {
-    "Authorization": process.env.AT as string,
-  }
-});
+    autoConnect: false,
+  });
 
 export const CheckAuth = () => {
   useEffect(() => {
-    localStorage.setItem("accessToken", process.env.AT as string);
-    localStorage.setItem("refreshToken", process.env.RT as string);
-
-
-    console.log(`socket: `, socket);
-    
+    socket.auth = {
+      "token": localStorage.getItem("accessToken") as string,
+    }
     socket.connect();
 
     socket.on("connect", () => {
@@ -31,10 +25,9 @@ export const CheckAuth = () => {
     return () => {
       socket.disconnect();
       console.log(`disconnected by unmounting`);
-      
     }
   }, []);
-  
+
   return (
     <></>
   )
--- src/app/main/right-wrapper/page.tsx
+++ src/app/main/right-wrapper/page.tsx
@@ -1,8 +1,10 @@
+import { socket } from '../components/CheckAuth'
 import Styles from './right-wrapper.module.scss'
 import UserList from './user-list/page'
 import UserProfile from './user-profile/page'
 
 export default function RightWrapper({ channelId }: { channelId: number | null }) {
+
     return (
         <div className={Styles.rightWrapper}>
             <UserProfile />
