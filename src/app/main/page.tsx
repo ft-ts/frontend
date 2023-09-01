21238eeb9c14fa51244dbf76801fd0aa5aad77d4
jeongmino <jeongmino1207@gmail.com>
2023-09-01 17:38:32 +0900

    feat: tokens are stored at local storage

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,16 +1,46 @@
+'use client'
+
+import React, { useEffect } from 'react';
 import LeftWrapper from './left-wrapper/page';
 import MidWrapper from './mid-wrapper/page';
 import RightWrapper from './right-wrapper/page';
-import { UserInfoContainer, UserProps, MainProps} from './userType';
 import './main.css';
 
-export default function Main() {
+function Main() {
+
+    useEffect(() => {
+        const getCookie = (name: string): string | undefined => {
+            const value = "; " + document.cookie;
+            const parts = value.split("; " + name + "=");
+            if (parts.length === 2) return parts.pop()?.split(";").shift();
+        }
+
+        const deleteCookie = (name: string) => {
+            document.cookie = name + '=; Max-Age=-99999999;'; 
+        }
+
+        const accessToken = getCookie('accessToken');
+        const refreshToken = getCookie('refreshToken');
+
+        if (accessToken) {
+            localStorage.setItem('accessToken', accessToken);
+            deleteCookie('accessToken');
+        }
+        if (refreshToken) {
+            localStorage.setItem('refreshToken', refreshToken);
+            deleteCookie('refreshToken');
+        }
+    }, []); 
 
     return (
-      <div id="main">
-        <LeftWrapper />
-        <MidWrapper />
-        <RightWrapper />
-      </div>
-    )
-  }
+        <div id="main">
+            <LeftWrapper />
+            <MidWrapper />
+            <RightWrapper />
+        </div>
+    );
+}
+
+export default Main;
+
+
