10c95c8f2651f31af553ce8d03af8743eff9e04e
Dohyun Lee <surimi.sake@gmail.com>
2023-09-03 01:53:33 +0900

    added : userMenu -> DM button Event
    - fixed : layout

--- src/app/main/right-wrapper/user-list/Components/UserListItem.tsx
+++ src/app/main/right-wrapper/user-list/Components/UserListItem.tsx
@@ -1,12 +1,20 @@
 'use client';
+import { useEffect, useState } from 'react';
 import Styles from './userListItem.module.scss';
+import Image from 'next/image';
 
-export function UserListItem() {
+export function UserListItem({item, state}: {item: any, state: any}) {
+    const [menuOn, setMenuOn, selectedUser, setSelectedUser] = state;
+
+    const toggleMenu = () => {
+        setMenuOn(!menuOn);
+        setSelectedUser({name: item.name, uid: item.uid});
+    }
     return (
-        <div className={Styles.userListItem}>
-            <div className={Styles.userListItemAvatar}></div>
-            <div className={Styles.userListItemName}>DOHYULEE</div>
-            <div className={Styles.userListItemStatus}>ONLINE</div>
+        <div onClick={toggleMenu} className={Styles.userListItem}>
+            <Image src={item.avatar} width={80} height={80} alt={item.name} className={Styles.userListItemAvatar}></Image>
+            <div className={Styles.userListItemName}>{item.name}</div>
+            <div className={Styles.userListItemStatus}>{item.status}</div>
         </div>
     )
 }
