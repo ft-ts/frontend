1fcba35ad4160c72d61c308b7a5fd0e07b11e3d1
김서우 <4nyth4me@gmail.com>
2023-09-21 21:50:12 +0900

    wip: to use http api

--- src/app/axios/client.ts
+++ src/app/axios/client.ts
@@ -34,42 +34,132 @@ apiClient.interceptors.response.use((response) => {
   return response;
 });
 
+/* src/app/main */
 export async function getMyInfo(): Promise<any> {
   return apiClient.get("/users");
 };
 
-export async function getUserList(): Promise<any> {
-  return apiClient.get("/users/all");
-}
-
+/* userList.page.tsx */
 export async function getUserListExceptMe(): Promise<any> {
   return apiClient.get("/users/all/except/me");
 }
 
+/*  userList.page.tsx */
 export async function getFreiendsList(): Promise<any> {
   return apiClient.get("/users/friends");
 }
 
+export async function postCreateChannel(title: string, mode: string, password: string): Promise<any> {
+  const payload = { title: title, mode: mode, password: password };
+  return await apiClient.post("/channels/create", payload);
+}
+
+export async function getChannelList(): Promise<any> {
+  return await apiClient.get("/channels/list/all");
+}
+
+export async function getMyChannelList(): Promise<any> {
+  return await apiClient.get("/channels/list/my");
+}
+
+/* channel.tsx */
+export async function getChannelProps(channelId : number): Promise<any> {
+  return await apiClient.get(`/channels/props/${channelId}`);
+}
+
+export async function getMyChannelRole(channelId : number): Promise<any> {
+  return await apiClient.get(`/channels/role/${channelId}`);
+}
+
+/*
+  channel.tsx
+*/
+export async function getChannelMembers(channelId : number): Promise<any> {
+  return await apiClient.get(`/channels/members/${channelId}`);
+}
+
+/*
+  dm.tsx
+  chatMenu.tsx
+  messageItem.tsx
+  userProfile.button.tsx
+*/
 export async function getUserByUid(uid: number): Promise<any> {
   return apiClient.get(`/users/${uid}`);
 }
 
+/*
+  game.search.tsx
+*/
 export async function getGameHistory(name: string): Promise<any> {
   return apiClient.get(`/pong/${name}`);
 }
 
+/*
+  dm.tsx
+*/
 export async function getDmLists(): Promise<any> {
   return apiClient.get(`/dm/list`);
 }
 
-export async function getDm(targetUid: number): Promise<any> {
-  return apiClient.get(`/dm/${targetUid}`);
+/*
+  chatRoom.tsx
+*/
+export async function getDirectMessages(uid: number): Promise<any> {
+  return await apiClient.get(`/dm/${uid}`);
 }
 
+export async function postDmRead(uid: number): Promise<any> {
+  return await apiClient.post(`/dm/read`, {data: {targetUid: uid}});
+}
+
+/*
+  chatRoom.tsx
+*/
+export async function getChannelMessages(channelId: number): Promise<any> {
+  return await apiClient.get(`/channels/log/${channelId}`);
+}
+
+export async function postBanUser(channelId: number, uid: number): Promise<any> {
+  const payload = { channelId: channelId, targetUid: uid};
+  return await apiClient.post(`/channels/ban`, payload);
+}
+
+export async function postUnbanUser(channelId: number, uid: number): Promise<any> {
+  const payload = { channelId: channelId, targetUid: uid};
+  return await apiClient.post(`/channels/unban`, payload);
+}
+
+export async function postKickUser(channelId: number, uid: number): Promise<any> {
+  const payload = { channelId: channelId, targetUid: uid};
+  return await apiClient.post(`/channels/kick`, payload);
+}
+
+export async function postInviteUser(channelId: number, uid: number): Promise<any> {
+  const payload = { channelId: channelId, targetUid: uid};
+  return await apiClient.post(`/channels/invite`, payload);
+}
+
+export async function postGrantAdmin(channelId: number, uid: number): Promise<any> {
+  const payload = { channelId: channelId, targetUid: uid};
+  return await apiClient.post(`/channels/grant/admin`, payload);
+}
+
+export async function postRevokeAdmin(channelId: number, uid: number): Promise<any> {
+  const paylaod = { channelId: channelId, targetUid: uid};
+  return await apiClient.post(`/channels/revoke/admin`, paylaod);
+}
+
+/*
+  userProfile.tsx
+*/
 export async function postFriend(uid: number): Promise<any> {
   return await apiClient.post(`/users/friends`, {data: {targetUid: uid}});
 }
 
+/*
+  userList.friend.tsx
+*/
 export async function deleteFriend(uid: number): Promise<any> {
   return await apiClient.delete(`/users/friends`, { data: { targetUid: uid } });
 }
