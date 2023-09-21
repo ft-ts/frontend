96a7bfcd507f1b25c06aa47876c08a5eae37f968
김서우 <4nyth4me@gmail.com>
2023-09-21 21:55:43 +0900

    styles: change get myInfo

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import MidWrapper from './mid-wrapper/mid-wrapper.page';
-import RightWrapper from './right-wrapper/right-wrapper.page';
-import { MainProps} from './userType';
+import { CheckAuth } from './components/CheckAuth';
+import { MainProps } from './userType';
 import './main.css';
 import React, { useState, useEffect } from 'react';
+import MidWrapper from './mid-wrapper/mid-wrapper.page';
+import RightWrapper from './right-wrapper/right-wrapper.page';
 import LeftWrapper from './left-wrapper/page';
-import { CheckAuth } from './components/CheckAuth';
+import { useGlobalContext } from '../Context/store';
+import { getMyInfo }  from '@/app/axios/client';
 
 function Main(props: MainProps) {
+  const { setMyInfo } : any = useGlobalContext();
+  const { setCurrentUser } : any = useGlobalContext();
+
+  useEffect(() => {
+    getMyInfo().then((res) => {
+      setMyInfo(res.data);
+      setCurrentUser(res.data);
+    });
+  }, []);
 
   return (
       <div id="main">
