6dfab3bdbca43a732eb9cf2bcc893c2c6cea2691
김서우 <4nyth4me@gmail.com>
2023-09-22 13:10:07 +0900

    fix: main page's confilct

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,16 +1,15 @@
 'use client';
 
-import { CheckAuth } from './components/CheckAuth';
-import { MainProps } from './userType';
 import './main.css';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import MidWrapper from './mid-wrapper/mid-wrapper.page';
 import RightWrapper from './right-wrapper/right-wrapper.page';
 import LeftWrapper from './left-wrapper/page';
+import { CheckAuth } from './components/CheckAuth';
 import { useGlobalContext } from '../Context/store';
 import { getMyInfo }  from '@/app/axios/client';
 
-function Main(props: MainProps) {
+function Main() {
   const { setMyInfo } : any = useGlobalContext();
   const { setCurrentUser } : any = useGlobalContext();
 
